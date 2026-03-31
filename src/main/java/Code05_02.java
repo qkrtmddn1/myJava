85fd14d25a68a480b93bb85ee236b0df9a3b66c5
qkrtmddn1 <parksw5002@gmail.com>
2026-03-31 10:20:37 +0900

    3/31

--- src/main/java/Code05_02.java
+++ src/main/java/Code05_02.java
@@ -2,8 +2,9 @@ public class Code05_02 {
     public static void main(String[] args) {
         int num= 99;
 
-        if (num < 100)
+        if (num < 100) {
             System.out.print("100보다 ");
             System.out.print("작습니다.");
+        }
     }
 }
