682ad7369b0e6d630d77211f8279828dbc684e8c
qkrtmddn1 <parksw5002@gmail.com>
2026-04-10 14:15:33 +0900

    4/10 짝수만 나오게

--- src/main/java/Code06_04.java
+++ src/main/java/Code06_04.java
@@ -1,6 +1,6 @@
 public class Code06_04 {
     public static void main(String[] args) {
-        for (int i =1 ; i  <= 10 ; i++ ) {
+        for (int i =2 ; i  <= 10 ; i += 2 ) {
             System.out.print(i + " ");
         }
     }
