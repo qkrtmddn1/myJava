1ef5f4865f8b43e349b9c7e3704266f04aab77ac
qkrtmddn1 <parksw5002@gmail.com>
2026-04-10 15:15:33 +0900

    4/10

--- src/main/java/Code06_12.java
+++ src/main/java/Code06_12.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Code06_12 {
-    public static void main(String[] agrs) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int hap = 0;
         int num1, num2;
